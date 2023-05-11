748fa9210df10401499afcf04197a28adb21d1a2
mgkim <lqaks456@gmail.com>
2023-05-11 17:24:14 +0900

    wsl

--- webcam/test.cpp
+++ webcam/test.cpp
@@ -7,13 +7,16 @@
 #include <unistd.h>
 #include <opencv2/opencv.hpp>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <chrono>
 
 #define PORT 8080
 
 using namespace std;
 using namespace cv;
 
-int main() {
+int main()
+{
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
@@ -21,13 +24,23 @@ int main() {
     char buffer[1024] = {0};
 
     // create socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
         perror("socket failed");
         return EXIT_FAILURE;
     }
 
     // set socket options
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        perror("setsockopt failed");
+        return EXIT_FAILURE;
+    }
+
+    // set socket receive buffer
+    int buf_size = 60000 * 1000;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_RCVBUF, &buf_size, sizeof(buf_size)))
+    {
         perror("setsockopt failed");
         return EXIT_FAILURE;
     }
@@ -38,54 +51,69 @@ int main() {
     address.sin_port = htons(PORT);
 
     // bind socket to address
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         perror("bind failed");
         return EXIT_FAILURE;
     }
 
     // listen for incoming connections
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 3) < 0)
+    {
         perror("listen failed");
         return EXIT_FAILURE;
     }
 
     // accept incoming connection
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    {
         perror("accept failed");
         return EXIT_FAILURE;
     }
 
     // receive images from client
-    int idx = 0;
-    while (true) 
+    //int idx = 0;
+    auto prev = chrono::steady_clock::now();
+    while (true)
     {
         // receive image size
         int img_size = 0;
-        if (read(new_socket, &img_size, sizeof(img_size)) <= 0) {
+        if (read(new_socket, &img_size, sizeof(int)) <= 0)
+        {
             perror("read failed");
             break;
         }
         cout << img_size << endl;
 
-        usleep(100 * 1000);
+        int bytes_available = 0;
+        while (bytes_available < img_size)
+            ioctl(new_socket, FIONREAD, &bytes_available);
 
-        // receive image data
         vector<uchar> data(img_size);
-        if (read(new_socket, data.data(), img_size) <= 0) {
+        if (read(new_socket, data.data(), img_size) <= 0)
+        {
             perror("read failed");
             break;
         }
 
         // decode image data
         Mat image = imdecode(data, IMREAD_COLOR);
-        if (image.empty()) {
+        if (image.empty())
+        {
             cerr << "Failed to decode image." << endl;
             break;
         }
 
-        char tmp[30];
-        sprintf(tmp, "./image%d.jpg", idx++);
-        imwrite(tmp,image);
+        //char tmp[30];
+        //sprintf(tmp, "./image%d.jpg", idx++);
+        //imwrite(tmp, image);
+        imshow("test", image);
+        waitKey(1);
+
+        auto now = chrono::steady_clock::now();
+        auto dt = chrono::duration_cast<chrono::milliseconds>(now - prev).count();
+        cout << "Elapsed time in milliseconds: " << dt << "ms" << endl;
+        prev = now;
     }
 
     // close sockets
