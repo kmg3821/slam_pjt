832ce2be6ff13f7273bea7a75dc228734e1d68f1
mgkim <lqaks456@gmail.com>
2023-05-13 23:36:00 +0900

    adjust parameters for camera

--- send/send.cpp
+++ send/send.cpp
@@ -54,21 +54,22 @@ int main()
         return EXIT_FAILURE;
     }
 
-    system("v4l2-ctl -c brightness=60");   // 0 ~ 100, default = 50
-    system("v4l2-ctl -c contrast=100");    // -100 ~ 100, default = 0
-    system("v4l2-ctl -c saturation=100");  // -100 ~ 100, default = 0
-    system("v4l2-ctl -c sharpness=100");   // 예리함 정도
-    system("v4l2-ctl -c rotate=180");      // 회전
+    system("v4l2-ctl -c brightness=85");  // 0 ~ 100, default = 50
+    system("v4l2-ctl -c contrast=80");   // -100 ~ 100, default = 0
+    system("v4l2-ctl -c saturation=80"); // -100 ~ 100, default = 0
+    system("v4l2-ctl -c sharpness=100");  // 예리함 정도
+    system("v4l2-ctl -c rotate=180");     // 회전
 
     system("v4l2-ctl -c auto_exposure=0");           // 0:auto, 1:manual
-    system("v4l2-ctl -c exposure_time_absolute=50"); // 1 ~ 10000, default = 1000
-    system("v4l2-ctl -c auto_exposure_bias=0");      // 노출 기준값
+    system("v4l2-ctl -c exposure_time_absolute=30"); // 1 ~ 10000, default = 1000
+    system("v4l2-ctl -c auto_exposure_bias=7");      // 노출 기준값
 
-    system("v4l2-ctl -c image_stabilization=1");  // 흔들림 방지
-    system("v4l2-ctl -c iso_sensitivity=4");      // 0 ~ 4, default = 0
-    system("v4l2-ctl -c iso_sensitivity_auto=1"); // 0:manual, 1:auto
+    system("v4l2-ctl -c image_stabilization=1");    // 흔들림 방지
+    system("v4l2-ctl -c iso_sensitivity=4");        // 0 ~ 4, default = 0
+    system("v4l2-ctl -c iso_sensitivity_auto=1");   // 0:manual, 1:auto
+    system("v4l2-ctl -c exposure_metering_mode=0"); // 0:average, 1:center, 2:spot, 3:matrix
 
-    cap.set(CV_CAP_PROP_FPS, 20);
+    system("v4l2-ctl -p 20"); // fps
 
     const auto t0 = chrono::high_resolution_clock::now();
     // for(int i = 0; i < 20; ++i)
