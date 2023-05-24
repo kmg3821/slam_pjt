343a3cd5ac5a0de3c9e98b1394a471bc45b48865
whitw <shkim7876@naver.com>
2023-05-24 14:53:31 +0900

    frame manager

--- controller-server/front/static/index.js
+++ controller-server/front/static/index.js
@@ -1,3 +1,24 @@
+const FRAME = 20;
+const imagePlaceholder = document.querySelector(".camera");
+const fpsPlaceholder = document.querySelector(".fps");
+
+class FPSManager{
+	constructor() {
+		this.capturedtime = []
+	}
+	capture(){
+		this.capturedtime.push(Date.now())
+	}
+	getFPS(){
+		const now = Date.now();
+		const start = now - 1000;
+		while(this.capturedtime[0] < start){
+			this.capturedtime.shift();
+		}
+		return this.capturedtime.length;
+	}
+}
+
 const getEnv = function(){
 	const promise = axios.get('/back')
 	const data = promise.then((res)=>{
@@ -7,7 +28,6 @@ const getEnv = function(){
 }
 
 const getImage = async function(backend){
-	const imagePlaceholder = document.querySelector(".camera");
 	let ok = false;
 	let imageUrl = null;
 	await axios.get(backend + "/image/recent")
@@ -15,19 +35,27 @@ const getImage = async function(backend){
 		if(res.status === 200){
 			imageUrl = res.data['image'];
 			imagePlaceholder.src = backend + '/' + imageUrl;
+			ok = true;
+		}
+		else if(res.status !== 304){
+			ok = false;
 		}
 	})
 	.catch((error)=>{
 		ok = false;
 	});
-	if(ok){
-		const image = await axios.get(backend + imageUrl);
-		imagePlaceholder.style.backgroundImage = `url('${image}')`;
-	}
-	else{
-		imagePlaceholder.style.backgroundImage = `placeholder.png`;
-	}
-	setTimeout(getImage, "100", backend);
+	return ok;
+}
+
+const setFPS = (fps) =>{
+	fpsPlaceholder.textContent = fps;
+}
+
+const onFrame = async function(backend, fpsmanager){
+	const ok = await getImage(backend);
+	fpsmanager.capture();
+	setFPS(fpsmanager.getFPS());
+	setTimeout(onFrame, Math.floor(1000/FRAME), backend, fpsmanager);
 }
 
 const main = async function(){
@@ -64,7 +92,9 @@ const main = async function(){
 			axios.post(backend + "/event", body);
 		});
 	});
-	getImage(backend);
+	const fpsmanager = new FPSManager();
+	console.log(fpsmanager);
+	onFrame(backend, fpsmanager);
 }
 
 main();
